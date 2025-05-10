b6cc83ba47909e7da4ab086f2d5d92d0f129458f
Cristovao da Cruz e Silva <cbeiraod@cern.ch>
2025-05-10 23:49:07 +0200

    Updated LCG to a newer release, but still same major version

--- TestBeam/condor_at_lxplus/load_python39.sh
+++ TestBeam/condor_at_lxplus/load_python39.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
+#source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_104d/x86_64-el9-gcc13-opt/setup.sh # 3.9.12
