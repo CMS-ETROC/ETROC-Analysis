22df54aff67c1102db18a70f413ee6f8aaace3db
Jongho Lee <jongho.lee@cern.ch>
2025-05-13 21:08:43 +0200

    safety check, create a new file

--- TestBeam/condor_at_lxplus/reduce_number_of_track_candidates.py
+++ TestBeam/condor_at_lxplus/reduce_number_of_track_candidates.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import argparse
+import argparse, sys, re
 
 parser = argparse.ArgumentParser(
             prog='Reduce number of track candidates',
@@ -30,7 +30,26 @@ args = parser.parse_args()
 
 track_output_df = pd.read_csv(f'{args.file}')
 previous_num = track_output_df.shape[0]
+
+if previous_num < 1000:
+    print(f'Number of track combinations: {previous_num}')
+    print('This script is only recommended if you have too many track combinations (e.g., over 2000).')
+    sys.exit(1)
+
 track_output_df = track_output_df.loc[track_output_df['count'] > args.minimum_ntracks]
 track_output_df.reset_index(drop=True, inplace=True)
-track_output_df.to_csv(f'{args.file}', index=False)
+
+prefix = args.file.split('_tracks')[0] ## prefix will not be changed
+
+# Check if input already has a reduced_<N>_time pattern
+match = re.match(r"(.+)_reduced_(\d+)_time$", args.file.split('.')[0])
+
+if match:
+    count = int(match.group(2)) + 1
+    suffix = f"_tracks_reduced_{count}_time"
+else:
+    suffix = f"_tracks_reduced_1_time"
+
+track_output_df.to_csv(f'{prefix}{suffix}.csv', index=False)
+print(f'New file {prefix}{suffix}.csv is created')
 print(f'Number of track combinations has been decreased from {previous_num} to {track_output_df.shape[0]}')
