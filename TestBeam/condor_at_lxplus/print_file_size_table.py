41228ed9a0c4c3ed490d7a44848ce8a87cb470a4
Jongho Lee <jongho.lee@cern.ch>
2025-05-15 23:42:52 +0200

    print total events and hits, add tqdm

--- TestBeam/condor_at_lxplus/print_file_size_table.py
+++ TestBeam/condor_at_lxplus/print_file_size_table.py
@@ -3,6 +3,7 @@
 from natsort import natsorted
 from tabulate import tabulate
 from pathlib import Path
+from tqdm import tqdm
 import argparse
 import pandas as pd
 
@@ -18,21 +19,26 @@ parser.add_argument(
     type = str,
     help = 'input directory name',
     required = True,
-    dest = 'dirname',
+    dest = 'input_dir',
 )
 
 args = parser.parse_args()
 
 columns_to_read = ['evt']
-files = natsorted(Path(args.input_dir)).glob('loop*feather')
+files = natsorted(Path(args.input_dir).glob('loop*feather'))
 outputs = {}
 
-for ifile in files:
+total_nevt = 0
+total_nhits = 0
+for ifile in tqdm(files):
     tmp_df = pd.read_feather(ifile, columns=columns_to_read)
     nevt = tmp_df['evt'].nunique()
     nrows = tmp_df.shape[0]
 
     outputs[ifile.name] = [nevt, nrows]
+    total_nevt += nevt
+    total_nhits += nrows
 
+outputs['total'] = [total_nevt, total_nhits]
 table_data = [[x, outputs[x][0], outputs[x][1]] for x in outputs.keys()]
 print(tabulate(table_data, headers=['File name', 'Number of events', 'Number of hits']))
