991aaf1e0817197690a6344096ee82c76079bb73
Jongho Lee <jongapples@gmail.com>
2026-05-15 11:55:59 +0200

    clean up

--- TestBeam/condor_at_lxplus/submit/submit_decoding.py
+++ TestBeam/condor_at_lxplus/submit/submit_decoding.py
@@ -14,10 +14,8 @@ echo ""
 
 source /cvmfs/sft.cern.ch/lcg/views/LCG_104a/x86_64-el9-gcc13-opt/setup.sh
 
-# Directory name uses $2 (index) and $1 (ClusterID)
 mkdir -p ./job_${1}_${2}
 
-# IMPORTANT: We do NOT quote $3 so bash splits the file bundle into individual names
 for fname in $3; do
     echo "Transferring ${fname}..."
     xrdcp -s root://eosuser.cern.ch/{{ eos_path }}/${fname} ./job_${1}_${2}
@@ -26,78 +24,59 @@ done
 tar -xf python_lib.tar
 export PYTHONPATH=${PWD}/local/lib/python3.9/site-packages:$PYTHONPATH
 
-# Output loop index is now strictly numeric (no commas)
-echo "python decoding.py -d job_${1}_${2} -o loop_${2}"
 python decoding.py -d job_${1}_${2} -o loop_${2}
-
 rm -r job_${1}_${2}
 """
-    options = {
-        'eos_path': input_dir_path,
-    }
-    return Template(bash_template).render(options)
+    return Template(bash_template).render(eos_path=input_dir_path)
 
 
-def load_jdl_template(condor_log_dir, eos_base_dir, output_dir, condor_scripts_dir):
-
-    jdl = """universe              = vanilla
-executable            = {3}/run_decode.sh
+def load_jdl_template(log_dir, eos_base, out_dir, scripts_dir):
+    jdl_template = """universe              = vanilla
+executable            = {{ scripts_dir }}/run_decode.sh
 should_Transfer_Files = YES
 whenToTransferOutput  = ON_EXIT
-# $1:ClusterId, $2:Index, $3:FileBundle
 arguments             = $(ClusterId) $(index) $(flist)
 transfer_Input_Files  = core/decoding.py, utils/python_lib.tar
-output                = {0}/$(ClusterId).$(ProcId).decoding.stdout
-error                 = {0}/$(ClusterId).$(ProcId).decoding.stderr
-log                   = {0}/decoding.log
+output                = {{ log_dir }}/$(ClusterId).$(ProcId).decoding.stdout
+error                 = {{ log_dir }}/$(ClusterId).$(ProcId).decoding.stderr
+log                   = {{ log_dir }}/decoding.log
 transfer_Output_Files = hits, status
-output_destination    = root://eosuser.cern.ch/{1}/{2}
+output_destination    = root://eosuser.cern.ch/{{ eos_base }}/{{ out_dir }}
 MY.XRDCP_CREATE_DIR   = True
 MY.WantOS             = "el9"
 +JobFlavour           = "workday"
-Queue index, flist from {3}/input_list.txt
-""".format(condor_log_dir, eos_base_dir, output_dir, condor_scripts_dir)
-    return jdl
+Queue index, flist from {{ scripts_dir }}/input_list.txt
+"""
+    return Template(jdl_template).render(
+        log_dir=log_dir,
+        eos_base=eos_base,
+        out_dir=out_dir,
+        scripts_dir=scripts_dir
+    )
 
 def make_jobs(args, log_dir, condor_scripts_dir):
 
-    # --- Setup Environments ---
     username = getpass.getuser()
     eos_base_dir = f'/eos/user/{username[0]}/{username}'
+    input_path = Path(eos_base_dir) / args.input_dir
 
-    input_path = Path(f'{eos_base_dir}/{args.input_dir}')
-    file_extension = None
-    file_list = None
-
-    bin_files = natsorted(input_path.glob('file*.bin'))
-    if bin_files:
-        print("Found '.bin' files.")
+    # Detect files once and keep them
+    file_list = natsorted(input_path.glob('file*.bin'))
+    if file_list:
         file_extension = 'bin'
-        file_list = bin_files
     else:
-        # If no .bin files, check for '_CE.dat' files.
-        dat_files = natsorted(input_path.glob('file*_CE.dat'))
-        if dat_files:
-            print("Found '_CE.dat' files.")
-            file_extension = 'dat'
-            file_list = dat_files
+        file_list = natsorted(input_path.glob('file*_CE.dat'))
+        file_extension = 'dat'
 
-    if file_list is None:
-        print(f"!!! ERROR: No 'file*.bin' or 'file*_CE.dat' files found in '{input_path}'. Exiting.")
+    if not file_list:
+        print(f"!!! ERROR: No valid files found in '{input_path}'. Exiting.")
         sys.exit(1)
 
     print(f"--> Auto-detected and using file extension: '{file_extension}'")
+    print(f'First file: {file_list[0].name}, Last file: {file_list[-1].name}')
 
-    glob_pattern = f'file*.{file_extension}'
-    file_list = natsorted(input_path.glob(glob_pattern))
-    print(f'\nFirst file: {file_list[0].name}')
-    print(f'Last file: {file_list[-1].name}')
-
-    listfile = condor_scripts_dir / f'input_list.txt'
-    if listfile.is_file():
-        listfile.unlink()
-
-    with open(listfile, 'w') as f:
+    input_list = condor_scripts_dir / f'input_list.txt'
+    with open(input_list, 'w') as f:
         for file_path in file_list:
             try:
                 physical_idx = int(file_path.name.split('.')[0].split('_')[1])
@@ -107,16 +86,20 @@ def make_jobs(args, log_dir, condor_scripts_dir):
 
             f.write(f"{physical_idx} {file_path.name}\n")
 
-    bash_script = load_bash_template(str(input_path))
+    bash_script = load_bash_template(input_path)
 
-    with open(condor_scripts_dir / f'run_decode.sh','w') as bashfile:
+    with open(condor_scripts_dir / 'run_decode.sh', 'w') as bashfile:
         bashfile.write(bash_script)
 
-    outdir = f'{args.output}'
-    jdl_script = load_jdl_template(condor_log_dir=log_dir, eos_base_dir=eos_base_dir, output_dir=outdir, condor_scripts_dir=condor_scripts_dir)
-    with open(condor_scripts_dir / f'condor_decoding.jdl','w') as jdlfile:
-        jdlfile.write(jdl_script)
+    jdl_script = load_jdl_template(
+        log_dir=log_dir,
+        eos_base=eos_base_dir,
+        out_dir=args.output,
+        scripts_dir=condor_scripts_dir
+    )
 
+    with open(condor_scripts_dir / 'condor_decoding.jdl', 'w') as jdlfile:
+        jdlfile.write(jdl_script)
 
 ## --------------------------------------
 if __name__ == "__main__":
@@ -179,20 +162,9 @@ if __name__ == "__main__":
     make_jobs(args=args, log_dir=log_dir, condor_scripts_dir=condor_scripts_dir)
 
     if args.dryrun:
-        input_txt_path = condor_scripts_dir / f"input_list.txt"
-        print('\n=========== Input text file ===========')
-        print('First 3 lines:')
-        subprocess.run(f"head -n 3 {input_txt_path}", shell=True)
-        print('Last 3 lines:')
-        subprocess.run(f"tail -n 3 {input_txt_path}", shell=True)
-        print()
-        print('=========== Bash file ===========')
-        with open(condor_scripts_dir / f"run_decode.sh") as f:
-            print(f.read(), '\n')
-        print('=========== Condor Job Description file ===========')
-        with open(condor_scripts_dir / f'condor_decoding.jdl') as f:
-            print(f.read(), '\n')
-        print()
+        print(f"[Dry Run] JDL: {condor_scripts_dir / 'condor_decoding.jdl'}")
+        print(f"[Dry Run] Bash: {condor_scripts_dir / 'run_decode.sh'}")
+        print(f"[Dry Run] Input: {condor_scripts_dir / 'input_list.txt'}")
 
     else:
         subprocess.run(['condor_submit', f'{condor_scripts_dir}/condor_decoding.jdl'])
